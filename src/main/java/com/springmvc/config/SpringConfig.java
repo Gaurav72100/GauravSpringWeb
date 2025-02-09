1888428f142d3663325a47df8ca39a6cc5b224aa
Gaurav72100 <gk240662@gmail.com>
2025-02-09 16:32:19 +0530

    update

--- src/main/java/com/springmvc/config/SpringConfig.java
+++ src/main/java/com/springmvc/config/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.springmvc.config;
 
+import java.util.List;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -8,22 +9,30 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 
-
+@EnableWebMvc
 @EnableTransactionManagement
 @Configuration
 @ComponentScan(basePackages="com.springmvc")
 public class SpringConfig implements WebMvcConfigurer {
+	
+	 @Override
+	    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+	        converters.add(new MappingJackson2HttpMessageConverter());
+	    }
 	
 	@Bean
 	public ViewResolver viewResolver() {
--- src/main/java/com/springmvc/controller/EmployeeController.java
+++ src/main/java/com/springmvc/controller/EmployeeController.java
@@ -4,6 +4,7 @@ package com.springmvc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,7 +20,6 @@ import com.springmvc.service.EmployeeService;
 
 @Controller
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/i")
 public class EmployeeController {
 	
 	
@@ -28,16 +27,16 @@ public class EmployeeController {
 	private EmployeeService employeeservice;
 	
 	
-	@GetMapping("/employees")
-	public String listEmployees(Model model) {
-		model.addAttribute("employees", employeeservice.getAllEmployees() );
-		return "employeeList";
-	}
+//	@GetMapping("/employees")
+//	public String listEmployees(Model model) {
+//		model.addAttribute("employees", employeeservice.getAllEmployees() );
+//		return "employeeList";
+//	}
 	
-	@GetMapping(value = "/emp")
+	@GetMapping( value="/emp",consumes=MediaType.ALL_VALUE ,produces=MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public List<Employee> getEmp(){
-		
+	System.out.println("Calling.....");
 		return  employeeservice.getAllEmployees();
 	}
 	 
@@ -51,42 +50,37 @@ public class EmployeeController {
 		return "Successfull";
 	}
 	
-	@GetMapping("/delEmployee/{id}")
-	public String deleteData(@PathVariable ("id") int id) {
-		employeeservice.deleteById(id);
-		return "redirect:/employees";
-	}
+//	@GetMapping("/delEmployee/{id}")
+//	public String deleteData(@PathVariable ("id") int id) {
+//		employeeservice.deleteById(id);
+//		return "redirect:/employees";
+//	}
 	
 	
-	@GetMapping("/login") 
+	@PostMapping("/login") 
 	@ResponseBody
-	public String login(@RequestParam String email) {
-       
+	public String login(@RequestParam String email) { 
 		for(String emp:employeeservice.emails()) {
 			if(emp.equals(email)) {
-				System.out.println("Found"+ emp);
-				break;
-			}
-			else {
-				System.out.println("NOt Found");
+				return emp;
 			}
 		}
-		return "Successful" ;
+		return "Please signup first" ;
 	}
 	 
-	 @GetMapping("/edit/{id}")
-	    public String showEditUserForm(@PathVariable("id") int id, Model model) {
-	        Employee employee = employeeservice.getUserById(id);
-	        model.addAttribute("employee", employee);
-	        return "employeeForm";
-	    }
+//	 @GetMapping("/edit/{id}")
+//	    public String showEditUserForm(@PathVariable("id") int id, Model model) {
+//	        Employee employee = employeeservice.getUserById(id);
+//	        model.addAttribute("employee", employee);
+//	        return "employeeForm";
+//	    }
 
-	@PostMapping("/updateEmployee")
-		public String updateVal(@ModelAttribute("employee") Employee employee) {
-			employeeservice.updatedEmpl(employee);
-			//model.addAttribute("employee");
-			return "redirect:/employees";
-		}
+//	@PostMapping("/updateEmployee")
+//		public String updateVal(@ModelAttribute("employee") Employee employee) {
+//			employeeservice.updatedEmpl(employee);
+//			//model.addAttribute("employee");
+//			return "redirect:/employees";
+//		}
 	
 
 }
